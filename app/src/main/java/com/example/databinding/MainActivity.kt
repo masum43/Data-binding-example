06bf28d95d34bd43237800745aeaa55ac1067ff1
masum <masum@iotait.tech>
2021-01-06 10:29:56 +0600

    data binding

--- app/src/main/java/com/example/databinding/MainActivity.kt
+++ app/src/main/java/com/example/databinding/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.databinding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.databinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.tv.text = "Data binding test"
+
     }
 }
